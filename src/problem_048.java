3f49fd638ef9eba9963be70551409abc9a31873b
jongbin oh <ohyecloudy@gmail.com>
2012-10-09 09:36:26 +0900

    problem 48

---
+++ src/problem_048.java
@@ -0,0 +1,26 @@
+/**
+ * Project Euler - problem 48
+ *   http://projecteuler.net/problem=48
+ * 
+ * @author ohyecloudy
+ *   https://github.com/ohyecloudy/project-euler-solutions
+ */
+
+import java.math.BigInteger;
+
+public class problem_048 {
+
+	public static void main(String[] args) {
+		BigInteger sum = new BigInteger("0");
+		for (int i = 1; i <= 1000; ++i) {
+			BigInteger bi = new BigInteger(Integer.toString(i));
+			bi = bi.pow(i);
+			
+			sum = sum.add(bi);
+		}
+		
+		String ret = sum.toString();
+		System.out.println(ret.substring(ret.length() - 10));
+	}
+
+}
---
+++ src/problem_048_test.java
@@ -0,0 +1,32 @@
+/**
+ * Project Euler - problem 48
+ *   http://projecteuler.net/problem=48
+ * 
+ * @author ohyecloudy
+ *   https://github.com/ohyecloudy/project-euler-solutions
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.math.BigInteger;
+
+public class problem_048_test {
+
+	@Test
+	public void testBigInteger() {
+		BigInteger bi1 = new BigInteger("2");
+		assertEquals("1024", bi1.pow(10).toString());
+		assertEquals("BigInteger는 immutable하기 때문", "2", bi1.toString());
+		
+		BigInteger bi2 = new BigInteger("2048");
+		assertEquals("2050", (bi1.add(bi2)).toString());
+	}
+	
+	@Test
+	public void testSubstr() {
+		String str = "abcdefghijklmnopqrstuvwxyz";
+		assertEquals("qrstuvwxyz", str.substring(str.length() - 10));
+	}
+
+}
