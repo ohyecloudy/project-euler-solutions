7db3853105036a41546bab61de32512fd6321ded
Jongbin Oh <ohyecloudy@gmail.com>
2013-05-09 11:55:56 +0900

    clojure > project.clj 추가

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject temp "0.1.0-SNAPSHOT"
+  :description "solutions for project euler"
+  :url "https://github.com/ohyecloudy/project-euler-solutions"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
+
